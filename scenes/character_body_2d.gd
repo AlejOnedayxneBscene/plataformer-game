58a6bc79a8ee07dcb7b470c974fba237e22e083a
Alejandro Guzmán <62736165+AlejOnedayxneBscene@users.noreply.github.com>
2026-02-19 22:32:01 -0500

    Initial commit

---
+++ scenes/character_body_2d.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+const SPEED = 300.0 # Bajamos un poco la velocidad para mejor control 
+const JUMP_VELOCITY = -550.0
+
+# Obtenemos la gravedad del proyecto
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var sprite_2d = $Sprite2D
+
+func _physics_process(delta):
+	# 1. Aplicar Gravedad 
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		# IMPORTANTE: Reseteamos la velocidad en Y si está en el suelo 
+		# para evitar que la gravedad se acumule
+		velocity.y = 0 
+
+	# 2. Manejar Salto 
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# 3. Movimiento Horizontal ↔️
+	var direction = Input.get_axis("left", "right")
+	
+	if direction != 0:
+		velocity.x = direction * SPEED
+		sprite_2d.flip_h = (direction < 0) # Voltear el sprite según dirección
+	else:
+		# Frenamos usando SPEED para que se detenga en seco y no desaparezca
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# 4. Control de Animaciones 
+	update_animations(direction)
+
+	# 5. Ejecutar Movimiento
+	move_and_slide()
+
+func update_animations(direction):
+	if not is_on_floor():
+		sprite_2d.play("Jump")
+	elif direction != 0:
+		sprite_2d.play("Run")
+	else:
+		sprite_2d.play("Idle")

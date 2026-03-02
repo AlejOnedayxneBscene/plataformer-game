efe7018fcd314c24afd93402b1bd00f9ce435688
Alejandro Guzmán <62736165+AlejOnedayxneBscene@users.noreply.github.com>
2026-03-01 20:11:42 -0500

    Se realizo la implementacion de colliders de la guia 2

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,36 +1,46 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -700.0
-@onready var sprite_2d = $Sprite2D
+const SPEED = 300.0 # Bajamos un poco la velocidad para mejor control 
+const JUMP_VELOCITY = -550.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+# Obtenemos la gravedad del proyecto
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
-	# Add run animation
-	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "Run"
-	else:
-		sprite_2d.animation = "Idle"
+@onready var sprite_2d = $Sprite2D
 
-	# Add the gravity.
+func _physics_process(delta):
+	# 1. Aplicar Gravedad 
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		sprite_2d.animation = "Jump"
+	else:
+		# IMPORTANTE: Reseteamos la velocidad en Y si está en el suelo 
+		# para evitar que la gravedad se acumule
+		velocity.y = 0 
 
-	# Handle jump.
+	# 2. Manejar Salto 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# 3. Movimiento Horizontal ↔️
 	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
-
-	move_and_slide()
 	
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
+	if direction != 0:
+		velocity.x = direction * SPEED
+		sprite_2d.flip_h = (direction < 0) # Voltear el sprite según dirección
+	else:
+		# Frenamos usando SPEED para que se detenga en seco y no desaparezca
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# 4. Control de Animaciones 
+	update_animations(direction)
+
+	# 5. Ejecutar Movimiento
+	move_and_slide()
+
+func update_animations(direction):
+	if not is_on_floor():
+		sprite_2d.play("Jump")
+	elif direction != 0:
+		sprite_2d.play("Run")
+	else:
+		sprite_2d.play("Idle")

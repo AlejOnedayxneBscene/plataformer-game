3365d8519696f27420488413d20d279b3fbdbc78
Alejandro Guzmán <62736165+AlejOnedayxneBscene@users.noreply.github.com>
2026-03-24 21:32:16 -0500

    guia 5

--- game_manager.gd
+++ game_manager.gd
@@ -1,8 +1,9 @@
 extends Node2D
-
+@onready var points_label = $"../ui/Panel/PointsLabel"
 
 var puntos = 0
 
 func add_puntos():
 	puntos += 1
+	points_label.text = "Puntos: " + str(puntos)
 	print(puntos)
